8948a8958d3bf53701d38460166808a798f01004
Silvit <silvi.cornejoo@gmail.com>
2025-09-26 11:09:31 +0200

    initial commit

---
+++ cypress/e2e/tests/todo.cy.js
@@ -0,0 +1,7 @@
+  describe('Navigate to Vendors Page', () => {
+    it('Visit the Vendors Page across the top nav bar', () => {
+    cy.visit('https://www.theknot.com/marketplace/wedding-djs-hoboken-nj?sort=featured', { failOnStatusCode: false })
+    cy.get('nav').contains('Vendors').click()
+      })
+    })
+
